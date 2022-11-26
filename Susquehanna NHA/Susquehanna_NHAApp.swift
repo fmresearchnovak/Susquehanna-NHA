0c07961c4feb8cb8a09843a176d17345a3cdca29
Ed <enovak@fandm.edu>
2022-11-26 09:00:11 -0500

    Initial Commit

---
+++ Susquehanna NHA/Susquehanna_NHAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Susquehanna_NHAApp.swift
+//  Susquehanna NHA
+//
+//  Created by Ed Novak on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Susquehanna_NHAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
